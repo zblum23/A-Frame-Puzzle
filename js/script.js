13cf626b2014c57463ac6235ca94d24c5cb808b3
zblum23 <122048948+zblum23@users.noreply.github.com>
2023-03-16 10:02:05 -0400

    with new java script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-alert("hello world");
+AFRAME.registerComponent('update-score-text', {
+    schema: { type: 'string'
+        
+    },
+
+    init: function () {
+      // Do something when component first attached.
+      var stringtoprint = this.data;
+      var testBox = document.querySelector("#score-text");
+      testBox.setAttribute('value', stringtoprint);
+    },
+
+    update: function () {
+      // Do something when component's data is updated.
+    },
+
+    remove: function () {
+      // Do something the component or its entity is detached.
+    },
+
+    tick: function (time, timeDelta) {
+      // Do something on every scene tick or frame.
+    }
+});
