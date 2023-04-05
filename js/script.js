979ed8755616383224fb5c9d8decc63220a9c2f6
zblum23 <122048948+zblum23@users.noreply.github.com>
2023-04-05 15:10:53 -0400

    final

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
-AFRAME.registerComponent('update-score-text', {
-    schema: { type: 'string'
-        
-    },
-
-    init: function () {
-      // Do something when component first attached.
-      var stringtoprint = this.data;
-      var testBox = document.querySelector("#score-text");
-      testBox.setAttribute('value', stringtoprint);
-    },
-
-    update: function () {
-      // Do something when component's data is updated.
-    },
-
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
-
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
-
-      
-    }
-
-    
-});
+ 
+  const greenBox = document.querySelector("#green");
+  const doorBox = document.querySelector("#door");
+  
+  greenBox.addEventListener("click", function() {
+    doorBox.setAttribute("visible", false);
+  })
